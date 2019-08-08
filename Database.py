178021262af8e1be723f39e2a611f3ab0da96641
Jakub Medek <medekjak@gmail.com>
2019-08-08 21:14:52 +0200

    Refactor, add get to database, check old price

--- Database.py
+++ Database.py
@@ -2,13 +2,14 @@ import json
 from enum import Enum
 from json import JSONDecodeError
 
-URL = 'url'
-NAME = 'name'
-PRICE = 'price'
-RATING = 'rating'
-DESCRIPTION = 'description'
 
 class DatabaseJSONFile:
+    URL = 'url'
+    NAME = 'name'
+    PRICE = 'price'
+    RATING = 'rating'
+    DESCRIPTION = 'description'
+
     def __init__(self, file, verbose):
         self.verbose = verbose
         self.file = file
@@ -22,14 +23,25 @@ class DatabaseJSONFile:
             return AddUpdateResult.FAILED
         ret_list = []
         for key in self.data:
-            ret_list.append(self.data[key][URL])
+            ret_list.append(self.data[key][self.URL])
         return ret_list
+
+    def get(self, url):
+        if not self.read_file():
+            return AddUpdateResult.FAILED
+        return self.data[url]
+
+    def get_all(self):
+        if not self.read_file():
+            return AddUpdateResult.FAILED
+        return self.data
 
     def add_update_data(self, url, name, price, rating, description):
         if not self.read_file():
             return AddUpdateResult.FAILED
         if url not in self.data:
-            self.data[url] = {URL: url, NAME: name, PRICE: price, RATING: rating, DESCRIPTION: description}
+            self.data[url] = {self.URL: url, self.NAME: name, self.PRICE: price,
+                              self.RATING: rating, self.DESCRIPTION: description}
             if not self.write_file():
                 return AddUpdateResult.FAILED
             result = AddUpdateResult.ADDED
@@ -43,20 +55,20 @@ class DatabaseJSONFile:
         result = AddUpdateResult.NOT_UPDATED
         if url in self.data:
             if name:
-                if self.data[url][NAME] != name:
-                    self.data[url][NAME] = name
+                if self.data[url][self.NAME] != name:
+                    self.data[url][self.NAME] = name
                     result = AddUpdateResult.UPDATED
             if price:
-                if self.data[url][PRICE] != price:
-                    self.data[url][PRICE] = price
+                if self.data[url][self.PRICE] != price:
+                    self.data[url][self.PRICE] = price
                     result = AddUpdateResult.UPDATED
             if rating:
-                if self.data[url][RATING] != rating:
-                    self.data[url][RATING] = rating
+                if self.data[url][self.RATING] != rating:
+                    self.data[url][self.RATING] = rating
                     result = AddUpdateResult.UPDATED
             if description:
-                if self.data[url][DESCRIPTION] != description:
-                    self.data[url][DESCRIPTION] = description
+                if self.data[url][self.DESCRIPTION] != description:
+                    self.data[url][self.DESCRIPTION] = description
                     result = AddUpdateResult.UPDATED
         if not self.write_file():
             return AddUpdateResult.FAILED
--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import sys
+
 from Database import DatabaseJSONFile, AddUpdateResult
 from DataGetter import DataGetter
 from joblib import Parallel, delayed
@@ -14,16 +16,26 @@ def get_data_parallelized(url):
 
 print('[INFO] Getting game URLs')
 urls = DataGetter.get_game_urls(gameListUrl, encoding)
+
 print('[INFO] Loading database')
 database = DatabaseJSONFile(database_file, False)
+
 print('[INFO] Getting all game data (might take a while!)')
 games = Parallel(n_jobs=32)(delayed(get_data_parallelized)(url) for url in urls)
+
 print('[INFO] Got all data, checking database')
+old_data = database.get_all()
+if old_data == AddUpdateResult.FAILED:
+    print('[ERROR] Reading database failed!')
+    sys.exit()
 for game in games:
     info = database.add_update_data(game.url, game.name, game.price, game.rating, game.description)
     if info == AddUpdateResult.ADDED:
-        print('NEW GAME: ' + game.name + ' (price: ' + str(game.price) + ' rating: ' + str(game.rating) + ' url: ' + game.url + ')')
+        print('NEW GAME: ' + game.name + ' (price: ' + str(game.price) + ' rating: ' + str(game.rating) + ' url: ' +
+              game.url + ')')
     elif info == AddUpdateResult.UPDATED:
-        print ('CHECK GAME (price change?): ' + game.name + ' (price: ' + str(game.price) + ' rating: ' + str(game.rating) + ' url: ' + game.url + ')')
+        if old_data[game.url][DatabaseJSONFile.PRICE] != game.price:
+            print ('PRICE CHANGE: ' + game.name + ' (price: ' + old_data[game.url][DatabaseJSONFile.PRICE] + '->' +
+                   str(game.price) + ' rating: ' + str(game.rating) + ' url: ' + game.url + ')')
     elif info == AddUpdateResult.FAILED:
         print('[ERROR] Adding game ' + game.name + ' failed!')
